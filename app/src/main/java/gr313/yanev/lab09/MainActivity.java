3d6bc9c4ee0f643776471182722574b45d002951
Blaze <blazedenjin@gmail.com>
2024-04-10 12:33:35 +0300

    Загрузка лабы 9

---
+++ app/src/main/java/gr313/yanev/lab09/MainActivity.java
@@ -0,0 +1,105 @@
+package gr313.yanev.lab09;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import math.interp;
+
+public class MainActivity extends AppCompatActivity {
+    String func = "cos";
+    float xMin;
+    float xMax;
+    int points;
+    MySurface s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        s = findViewById(R.id.mySurface);
+
+        xMin = intent.getFloatExtra("xMin", 0.0f);
+        xMax = intent.getFloatExtra("xMax", 0.0f);
+        if (intent.getIntExtra("points", 0) < 1) {
+            Toast.makeText(this, "Введите минимум одну точку", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        s.n = intent.getIntExtra("points", 0);
+        s.x = new float[s.n];
+        s.y = new float[s.n];
+        drawgraph();
+    }
+
+    public void on_createDialog_click(View v){
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.activity_alert);
+        RadioGroup rgr = (RadioGroup) dialog.findViewById(R.id.radgroup);
+        rgr.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (dialog.findViewById(R.id.radcos).getId() == checkedId){
+                    func = "cos";
+                }
+                if (dialog.findViewById(R.id.radsin).getId() == checkedId){
+                    func = "sin";
+                }
+                if (dialog.findViewById(R.id.radtan).getId() == checkedId){
+                    func = "tan";
+                }
+                if (dialog.findViewById(R.id.radacos).getId() == checkedId){
+                    func = "acos";
+                }
+
+                if (dialog.findViewById(R.id.radasin).getId() == checkedId){
+                    func = "asin";
+                }
+                drawgraph();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+    protected void drawgraph(){
+
+        MySurface s = findViewById(R.id.mySurface);
+        s.invalidate();
+        s.x = new float[s.n];
+        s.y = new float[s.n];
+        for (int i = 0; i < s.n; i++)
+        {
+            s.x[i] = interp.map(i, 0, s.n - 1, xMin, xMax);
+            if (func == "cos") s.y[i] = (float) Math.cos(s.x[i]);
+            if (func == "sin") s.y[i] = (float) Math.sin(s.x[i]);
+            if (func == "tan") s.y[i] = (float) Math.tan(s.x[i]);
+            if (func == "acos") s.y[i] = (float) Math.acos(s.x[i]);
+            if (func == "asin") s.y[i] = (float) Math.asin(s.x[i]);
+        }
+        s.update();
+        s.invalidate();
+    }
+    public void on_btnPoints_click(View v){
+        finish();
+    }
+    public void btnPlus(View v)
+    {
+        s.zoom_plus(2.f);
+        s.invalidate();
+    }
+
+    public void btnMinus(View v)
+    {
+        s.zoom_minus(2.f);
+        s.invalidate();
+    }
+}
---
+++ app/src/main/java/gr313/yanev/lab09/MySurface.java
@@ -0,0 +1,111 @@
+package gr313.yanev.lab09;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import math.arr;
+import math.interp;
+
+public class MySurface extends SurfaceView{
+    Paint p;
+    float tx, ty;
+    boolean tt;
+    float ax0, ax1, ay0, ay1;
+    float xmin;
+    float xmax;
+    float ymin;
+    float ymax;
+    float[] x;
+    float[] y;
+    int n;
+    float XMIN;
+    float XMAX ;
+    public void update(){
+        xmin = arr.min(x, n);
+        xmax = arr.max(x, n);
+        ymin = arr.min(y, n);
+        ymax = arr.max(y, n);
+        XMIN = xmin;
+        XMAX = xmax;
+    }
+
+    public MySurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        p = new Paint();
+        p.setColor(Color.RED);
+
+        setWillNotDraw(false);
+    }
+    protected void onDraw(Canvas canvas){
+        canvas.drawColor(Color.WHITE);
+        int w = canvas.getWidth();
+        int h = canvas.getHeight();
+
+        float x0 = 0.0f, y0 = 0.0f;
+        for (int i = 0; i < n; i++)
+        {
+            float x1 = interp.map(x[i], xmin, xmax, 0, w - 1);
+            float y1 = interp.map(y[i], ymin, ymax, h - 1, 0);
+
+            if (i > 0) canvas.drawLine(x0, y0, x1, y1, p);
+            x0 = x1;
+            y0 = y1;
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        int act = event.getAction();
+        float x = event.getX();
+        float y = event.getY();
+        switch (act)
+        {
+            case MotionEvent.ACTION_DOWN:
+                tx = x;
+                ty = y;
+                tt = true;
+                return true;
+            case MotionEvent.ACTION_UP:
+                tt=false;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+            {
+                float dx = x - tx;
+                float dy = y - ty;
+                dx *= -0.01f;
+                dy *= (ay1 - ay0) / getWidth();
+                ax0 += dx;
+                ax1 += dx;
+                ay0 += dy;
+                ay1 += dy;
+                tx = x;
+                ty = y;
+                invalidate();
+                return true;
+            }
+        }
+        return false;
+    }
+    public void zoom_plus(float numb) {
+        if (xmin + numb < xmax - numb) {
+            xmax -= numb;
+            xmin += numb;
+        }
+    }
+
+    public void zoom_minus(float numb) {
+        if (xmin - numb > XMIN && xmax + numb < XMAX) {
+            xmax += numb;
+            xmin -= numb;
+        }
+        else {
+            xmax = XMAX;
+            xmin = XMIN;
+        }
+    }
+}
